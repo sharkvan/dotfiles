540ed7c5beedee6e98ab056bbc0f74f18cf78a48
Florian Suess <me@floriansuess.com>
2019-10-02 17:04:09 +1300

    Provide install script for initial symlinks

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s -f ~/.dotfiles/.bash_profile          ~/.bash_profile
+ln -s -f ~/.dotfiles/.bashrc                ~/.bashrc
+ln -s -f ~/.dotfiles/.shell-prompt.sh       ~/.shell-prompt.sh
+
+ln -s -f ~/.dotfiles/.tmux.conf             ~/.tmux.conf
+ln -s -f ~/.dotfiles/.vimrc                 ~/.vimrc
+
