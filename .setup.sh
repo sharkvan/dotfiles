755f54f3a8c4f5bbf47401d1348dbc5252305ddd
Tim Schruben <sharkvan@users.noreply.github.com>
2019-11-13 01:21:44 -0500

    Update .setup.sh
    Execute vundle setup

--- .setup.sh
+++ .setup.sh
@@ -25,3 +25,5 @@ git clone --depth 1 https://github.com/tmux/tmux.git && \
     make install
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim -c 'PluginInstall' -c 'qa!'
